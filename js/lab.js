5f29ac43e00d0e8fd90ecd1fd847471dca75861f
jelledekok <99061618@mydavinci.nl>
2019-10-30 14:27:30 +0100

    oefenen

---
+++ js/lab.js
@@ -0,0 +1,14 @@
+function halloWereld(aantal){ 
+	for(var i=1; i <= aantal; i++)
+   		document.write(i + " Hallo Wereld! <br/>" );
+
+}
+
+halloWereld(3);
+halloWereld(7);
+var a = prompt('vul een getal in');
+var b = prompt('vul een getal in');
+function brekening(){
+for(var  a + b);
+}
+brekening();
